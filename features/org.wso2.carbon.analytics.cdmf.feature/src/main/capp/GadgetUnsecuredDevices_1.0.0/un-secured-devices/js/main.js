b656bb9442d07fe2379faa62a31d4d1c20819023
dilanua <dilan.u.ariyaratne@gmail.com>
2016-10-12 18:27:55 +0530

    Updating un-secured-devices gadget view

--- features/org.wso2.carbon.analytics.cdmf.feature/src/main/capp/GadgetUnsecuredDevices_1.0.0/un-secured-devices/js/main.js
+++ features/org.wso2.carbon.analytics.cdmf.feature/src/main/capp/GadgetUnsecuredDevices_1.0.0/un-secured-devices/js/main.js
@@ -38,6 +38,7 @@ vb.startPolling = function () {
         vb.fetch();
         vb.freeze = vb.selected_filter_groups.length > 0;
     }, 500);
+    //noinspection JSUnusedGlobalSymbols
     this.polling_task = setInterval(function () {
         vb.fetch();
     }, gadgetConfig.polling_interval);
@@ -48,42 +49,65 @@ vb.fetch = function () {
     vb.data.length = 0;
     vb.force_fetch = false;
 
+    //noinspection JSUnresolvedVariable
     wso2.gadgets.XMLHttpRequest.get(gadgetConfig.source,
-        function(response){
+        function (response) {
+            console.log(JSON.stringify(response));
             if (Object.prototype.toString.call(response) === '[object Array]' && response.length === 1) {
+                //noinspection JSUnresolvedVariable
                 vb.filter_context = response[0].groupingAttribute;
                 var data = response[0]["data"];
                 if (data && data.length > 0) {
-                    var non_complaint1 = document.getElementById('non-complaintSpan');
-                    non_complaint1.innerHTML = data[0].deviceCount;
-                    var non_complaint = document.getElementById('non-complaint');
-                    if (data[0].deviceCount == 0) {
-                        if (non_complaint.hasAttribute("onclick")){
-                            non_complaint.removeAttribute("onclick");
+                    //noinspection JSUnresolvedVariable
+                    var nonCompliantDeviceCount = data[0].deviceCount;
+                    var nonCompliantCountElm = "h1#non-compliant-count";
+                    var nonCompliantDivElm = "div#NON_COMPLIANT";
+                    if (nonCompliantDeviceCount > 0) {
+                        if (nonCompliantDeviceCount > 99) {
+                            $(nonCompliantCountElm).html("99<sup>+</sup>");
+                        } else {
+                            $(nonCompliantCountElm).html(nonCompliantDeviceCount.toString());
                         }
+                        $(nonCompliantCountElm).removeClass("text-success");
+                        $(nonCompliantCountElm).addClass("text-danger");
+                        $(nonCompliantDivElm).css("cursor", "pointer");
+                        $(nonCompliantCountElm).attr("onclick", "vb.onclick('" + data[0].group + "')");
                     } else {
-                        non_complaint.setAttribute("onclick", "vb.onclick2('"+data[0].group+"')");
+                        $(nonCompliantCountElm).removeClass("text-danger");
+                        $(nonCompliantCountElm).addClass("text-success");
+                        $(nonCompliantDivElm).css("cursor", "default");
+                        $(nonCompliantCountElm).removeAttr("onclick");
                     }
-                    var unmonitored1 = document.getElementById('unmonitoredSpan');
-                    unmonitored1.innerHTML = data[1].deviceCount;
-                    var unmonitored = document.getElementById('unmonitored');
-                    if (data[1].deviceCount == 0) {
-                        if (unmonitored.hasAttribute("onclick")){
-                            unmonitored.removeAttribute("onclick");
+                    //noinspection JSUnresolvedVariable
+                    var unmonitoredDeviceCount = data[1].deviceCount;
+                    var unmonitoredCountElm = "h1#unmonitored-count";
+                    var unmonitoredDivElm = "div#UNMONITORED";
+                    if (unmonitoredDeviceCount > 0) {
+                        if (unmonitoredDeviceCount > 99) {
+                            $(unmonitoredCountElm).html("99<sup>+</sup>");
+                        } else {
+                            $(unmonitoredCountElm).html(unmonitoredDeviceCount.toString());
                         }
+                        $(unmonitoredCountElm).removeClass("text-success");
+                        $(unmonitoredCountElm).addClass("text-warning");
+                        $(unmonitoredDivElm).css("cursor", "pointer");
+                        $(unmonitoredCountElm).attr("onclick", "vb.onclick('" + data[1].group + "')");
                     } else {
-                        unmonitored.setAttribute("onclick", "vb.onclick2('"+data[1].group+"')");
+                        $(unmonitoredCountElm).removeClass("text-warning");
+                        $(unmonitoredCountElm).addClass("text-success");
+                        $(unmonitoredDivElm).css("cursor", "default");
+                        $(unmonitoredCountElm).removeAttr("onclick");
                     }
                 }
             } else {
                 console.error("Invalid response structure found: " + JSON.stringify(response));
             }
-        }, function(){
+        }, function () {
             console.warn("Error accessing source for : " + gadgetConfig.id);
         });
 };
 
-vb.onclick2 = function (filterGroup) {
+vb.onclick = function (filterGroup) {
     var url = getBaseURL() + "devices?g_" + vb.filter_context + "=" + filterGroup;
     window.open(url);
 };
